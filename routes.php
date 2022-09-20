9d377fad521d4cef3b894d77205db07b49000c04
me42th <david.meth@hotmail.com>
2022-09-20 12:11:28 -0300

    Dia 1

--- routes.php
+++ routes.php
@@ -1,4 +1,13 @@
 <?php
-$page = ($_GET['page']??'login').'.view';
-$content = file_get_contents(VIEW_FOLDER.$page);
-echo $content;
+$page = $_GET['page']??'login';
+switch($page){
+    case 'login':
+        do_login();
+    break;
+    case 'register':
+        do_register();
+    break;
+    default:
+        do_not_found();
+    break;
+}
